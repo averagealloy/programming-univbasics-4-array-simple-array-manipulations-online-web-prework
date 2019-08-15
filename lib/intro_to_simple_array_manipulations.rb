75e5afa3cf348e00869c77d0bedd99aecface3f2
Mike McTighe <m.mctighe10@gmail.com>
2019-08-15 00:10:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -47,6 +47,6 @@ def using_delete(array, string )
   array.delete(string)
 end
 
-def using_delete_at(array,string)
-  array.delete(2)
+def using_delete_at(array,integer)
+  array.delete(integer)
 end
